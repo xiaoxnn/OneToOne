ef5fdfecad796e3e352148d2aeb9a62a7ad1d5c1
[justin] <[360342388@qq.com]>
2017-12-07 17:10:05 +0800

    更新数据

--- App.js
+++ App.js
@@ -26,6 +26,9 @@ export default class App extends Component<{}> {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
