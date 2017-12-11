9547ea34ab37dae0001486d274abb1eba0b85a9b
[justin] <[360342388@qq.com]>
2017-12-11 18:11:06 +0800

    video

--- android/app/src/main/java/com/oneto/MainApplication.java
+++ android/app/src/main/java/com/oneto/MainApplication.java
@@ -11,7 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 import com.brentvatne.react.ReactVideoPackage;
-
+import com.github.yamill.orientation.OrientationPackage;  // <--- import
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new ReactVideoPackage()
+          new ReactVideoPackage(),
+          new OrientationPackage()
 
       );
     }
