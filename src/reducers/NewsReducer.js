a2d93e557a46205f8568b2d4d0df1fd5d0b04077
[justin] <[360342388@qq.com]>
2017-12-15 18:05:09 +0800

    添加图片

---
+++ src/reducers/NewsReducer.js
@@ -0,0 +1,45 @@
+
+import * as TYPES  from './../actions/ActionTypes';
+import {RefreshState}  from '../components/common/RefreshListView'
+const initialstate={
+    status:'init',
+    dataSource:[],
+    page:1,
+    size:20,
+    loaded:false,   //判断页面是否有数据
+    empty:false,    //判断页面是否为空
+    connectfail:false,  //初始化连接服务器失败
+    refreshState: RefreshState.Idle,
+}
+
+export  default function  getData(state=initialstate,action){
+
+    switch (action.type){
+
+        case TYPES.load_success_video:
+            return Object.assign({},state,{
+                loaded:action.loaded,
+                dataSource:action.state==RefreshState.Idle||action.state==RefreshState.HeaderRefreshing?action.data:state.dataSource.concat(action.data),
+                page:action.state==RefreshState.Idle||action.state==RefreshState.FooterRefreshing?state.page+1:1,
+                refreshState: RefreshState.Idle,
+            })
+
+        case TYPES.load_success_empty:
+            return Object.assign({},state,{
+                empty:true,
+            })
+
+        case TYPES.load_fail_video:
+            return Object.assign({},state,{
+                refreshState: RefreshState.Failure,
+                connectfail:true,
+            })
+
+        case TYPES.refresh_state:
+            return Object.assign({},state,{
+                refreshState:action.state,
+            })
+        default:
+            return state;
+    }
+}
